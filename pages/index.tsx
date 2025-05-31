e2dbf1eb9cbfd141843c497ee6e1fb5169d81bc3
Chris Rapple <chris@legacymind.ai>
2025-05-31 16:49:27 -0400

    Add index page to trigger full frontend deployment

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", paddingTop: "100px", fontFamily: "sans-serif" }}>
+      <h1>Welcome to Simon AI</h1>
+      <p><a href="/simon">Talk to Simon</a></p>
+    </div>
+  );
+}
