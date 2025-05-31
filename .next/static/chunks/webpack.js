a88aebd39a498b4f888c541f116c383eecfbc4ae
Chris Rapple <chris@legacymind.ai>
2025-05-31 14:07:32 -0400

    Fix .gitignore and remove tracked node_modules

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -181,7 +181,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("55d26ae535028349")
+/******/ 		__webpack_require__.h = () => ("653521a37eba6515")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
